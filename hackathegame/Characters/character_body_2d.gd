8828ad8c1265f7d7f085aecd41a59983cce2b388
Stevuie <sh1746@scarletmail.rutgers.edu>
2024-10-27 01:59:49 -0400

    Onion on and on

--- hackathegame/Characters/character_body_2d.gd
+++ hackathegame/Characters/character_body_2d.gd
@@ -1,11 +1,6 @@
 extends CharacterBody2D
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-# oi
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _ready():
+	animation_player.play("Onion Idle")
